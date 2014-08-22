5535e4a2a762a637535f9a07f95dff2013d9401b
Josh Jeong <jcjeong@gmail.com>
2014-08-22 00:42:03 -0700

    Styling home page w css WIP

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+  var v = new View();
+  var c = new Controller(v);
+  c.bindListeners()
+})
+
+
+View = {}
+
+var View = function(){
+  this.changeMenuPosition = function(){
+    $('.workout-list').slideToggle()
+  }
+}
+
+Controller = {}
+
+var Controller = function(view){
+  v = view
+  this.bindListeners = function(){
+    $('.menu-button').on('click', this.showMenu);
+  };
+
+  this.showMenu = function(){
+    v.changeMenuPosition()
+  }
+
+}
