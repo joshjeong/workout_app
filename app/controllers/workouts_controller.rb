67282e25111a497b4f6bd58a9fe868138d23d229
Josh Jeong <jcjeong@gmail.com>
2014-08-25 15:33:31 -0700

    Fixes workout attribute values being displayed in table

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -11,13 +11,13 @@ class WorkoutsController < ApplicationController
     params.permit!
     @user = User.where('id = ?', params[:user_id])
     @user.first.workouts.create(params[:workout])
+    binding.pry
     redirect_to user_workouts_path
   end
 
   def show
     user_id = params[:user_id]
-    id = params[:id]
-    @workout = Workout.find(id)
+    @workout = Workout.find(params[:id])
     @workout_name = @workout.name
     @workouts = Workout.where('user_id = ? AND name = ?', user_id, @workout_name)
   end
