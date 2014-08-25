609701257bac391ef019c9d823d57556bdeb7e72
Josh Jeong <jcjeong@gmail.com>
2014-08-25 15:43:52 -0700

    fixes back button

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -11,15 +11,13 @@ class WorkoutsController < ApplicationController
     params.permit!
     @user = User.where('id = ?', params[:user_id])
     @user.first.workouts.create(params[:workout])
-    binding.pry
     redirect_to user_workouts_path
   end
 
   def show
     user_id = params[:user_id]
     @workout = Workout.find(params[:id])
-    @workout_name = @workout.name
-    @workouts = Workout.where('user_id = ? AND name = ?', user_id, @workout_name)
+    @workouts = Workout.where('user_id = ? AND name = ?', user_id, @workout.name)
   end
 
 end
