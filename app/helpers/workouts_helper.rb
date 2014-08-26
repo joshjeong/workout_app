7a25b8ba8b481fdef03be29aa7f000f11bca2c24
Josh Jeong <jcjeong@gmail.com>
2014-08-26 10:05:23 -0700

    Adds highcharts graph and precompiles for heroku

--- app/helpers/workouts_helper.rb
+++ app/helpers/workouts_helper.rb
@@ -2,4 +2,11 @@ module WorkoutsHelper
   def filter_result(attr_name)
     return attr_name != "user_id" && attr_name != "created_at" && attr_name != "updated_at" && attr_name != "name" && attr_name != "id"
   end
+
+  def get_date(workout)
+    day = workout.created_at.day
+    month = workout.created_at.month
+    year = workout.created_at.year
+    "#{month}/#{day}/#{year}"
+  end
 end
